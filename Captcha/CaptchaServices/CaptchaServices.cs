6376e921d812a224bea04ab2a4999b4085bfcbcc
Pourya.lh <Pourya.lh96@gmail.com>
2023-07-24 16:19:27 +0330

    --

---
+++ Captcha/CaptchaServices/CaptchaServices.cs
@@ -0,0 +1,32 @@
+﻿using Captcha.CaptchaDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Captcha.CaptchaServices
+{
+    public class CaptchaServices
+    {
+        private readonly CaptchaOptionsParams _params;
+
+        public CaptchaServices(CaptchaOptionsParams captchaOptionsParams)
+        {
+            _params = captchaOptionsParams;
+        }
+
+        private AffineTransformBuilder getRotation()
+        {
+            Random random = new Random();
+            var builder = new AffineTransformBuilder();
+            var width = random.Next(10, _params.Width);
+            var height = random.Next(10, _params.Height);
+            var pointF = new PointF(width, height);
+            var rotationDegrees = random.Next(0, _params.MaxRotationDegrees);
+            var result = builder.PrependRotationDegrees(rotationDegrees, pointF);
+            return result;
+        }
+
+    }
+}
